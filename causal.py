a1362b0f690a5ed256afd59a7f3af9a8fa374847
kdv123 <kdv123@gmail.com>
2024-08-27 13:53:35 -0400

    Added timing to predict for causal.

--- causal.py
+++ causal.py
@@ -331,12 +331,12 @@ class CausalLanguageModel(LanguageModel):
     def dump_predict_times(self) -> None:
         """Print some stats about the prediction timing"""
         print(f"Predict times: {self.predict_start_ns} {self.predict_search_ns} {self.predict_search_inner_ns} {self.predict_inference_ns} {self.predict_end_ns} {self.predict_total_ns}")
-        print(f"Predict %: {self.predict_start_ns / self.predict_total_ns * 100.0} "
-              f"{self.predict_search_ns / self.predict_total_ns * 100.0} "
-              f"{self.predict_search_inner_ns / self.predict_total_ns * 100.0} "
-              f"{self.predict_inference_ns / self.predict_total_ns * 100.0} "
-              f"{self.predict_end_ns / self.predict_total_ns * 100.0} "
-              f"{self.predict_total_ns / self.predict_total_ns * 100.0}")
+        print(f"Predict %: {self.predict_start_ns / self.predict_total_ns * 100.0:.1f} "
+              f"{self.predict_search_ns / self.predict_total_ns * 100.0:.1f} "
+              f"{self.predict_search_inner_ns / self.predict_total_ns * 100.0:.1f} "
+              f"{self.predict_inference_ns / self.predict_total_ns * 100.0:.1f} "
+              f"{self.predict_end_ns / self.predict_total_ns * 100.0:.1f} "
+              f"{self.predict_total_ns / self.predict_total_ns * 100.0:.1f}")
 
     def update(self) -> None:
         """Update the model state"""
