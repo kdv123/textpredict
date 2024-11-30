32382d27f17f33e581a5ab34798a860648b11162
Keith Vertanen <kdv123@gmail.com>
2024-11-30 08:49:35 -0700

    Added utility to dump subword token text of causal model

--- dump_tokens.py
+++ dump_tokens.py
@@ -14,15 +14,7 @@ if __name__ == "__main__":
 
     symbol_set = alphabet()
     lm = CausalLanguageModel(symbol_set=symbol_set,
-                             lang_model_name=args.model_name,
-                             lm_device="cpu",
-                             lm_path=args.model_dir,
-                             lm_left_context=args.left_context,
-                             beam_width=args.beam_width,
-                             fp16=args.fp16,
-                             mixed_case_context=args.mixed_case_context,
-                             case_simple=args.case_simple,
-                             max_completed=args.max_completed)
+                             lang_model_name=args.model_name)
 
     tokens = lm.get_all_tokens_text()
     for i, token in enumerate(tokens):
