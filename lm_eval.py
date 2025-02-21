becf11e3701111d4e7679aa329794d95228d1dde
Keith Vertanen <kdv123@gmail.com>
2025-02-21 13:25:19 -0600

    Add optional skipping of symbols

--- lm_eval.py
+++ lm_eval.py
@@ -316,7 +316,9 @@ if __name__ == "__main__":
 
             # Optional stripped of symbols not in our set
             if args.skip_oov_symbols:
+                print(f"tokens = {tokens}")
                 tokens_stripped = [token for token in tokens if token in symbol_set]
+                print(f"tokens_stripped = {tokens_stripped}")
                 skipped_symbols += len(tokens) - len(tokens_stripped)
                 tokens = tokens_stripped
 
