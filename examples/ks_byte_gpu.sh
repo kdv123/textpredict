fee5ed35ab303a6848fb1261973906f0cf7ea8a6
Keith Vertanen <kdv123@gmail.com>
2025-04-22 13:59:24 -0400

    added cuda to ks script

--- examples/ks_byte_gpu.sh
+++ examples/ks_byte_gpu.sh
@@ -1,4 +1,8 @@
 # Keystroke savings using a byte LLM to predict next words
+#
+# Parameters:                                                                                                                                                                
+#  $1 - index of the GPU to use
+CUDA=${1}
 
 # Result: cheetah, 2080Ti GPU, 4/22/25
 
@@ -6,4 +10,4 @@ MODEL_NAME=nllg/bygpt5-small-en
 PHRASES=comm_case_dev.txt
 wc -l -w ${PHRASES}
 
-../keystroke_savings.py --phrases ${PHRASES} --model-name ${MODEL_NAME} --byte --case-simple --nbest 5 --beam 2.0 --lower --strip --use-cuda --fp16
+CUDA_VISIBLE_DEVICES=${CUDA} ../keystroke_savings.py --phrases ${PHRASES} --model-name ${MODEL_NAME} --byte --case-simple --nbest 5 --beam 2.0 --lower --strip --use-cuda --fp16
