5ac8e39f32de4dc7f949bf51ae0afaa9fcc53fc5
Keith Vertanen <kdv123@gmail.com>
2025-04-22 15:24:02 -0400

    added primus result

--- examples/ks_byte_gpu.sh
+++ examples/ks_byte_gpu.sh
@@ -4,6 +4,12 @@
 #  $1 - index of the GPU to use
 CUDA=${1}
 
+# Result: primus, A100 GPU, 4/22/25
+#  TRUNCATED: 0
+#  TIME: 375.36
+#  SECS/PRED: 0.1718
+#  FINAL KS: 50.1536
+
 # Result: cheetah, 2080Ti GPU, 4/22/25
 
 MODEL_NAME=nllg/bygpt5-small-en
