305b86b04cbd8f1444cac51de7dd0fa6249745bc
Keith Vertanen <kdv123@gmail.com>
2025-04-23 10:02:28 -0400

    result for cpu byte

--- examples/ks_byte_cpu.sh
+++ examples/ks_byte_cpu.sh
@@ -1,6 +1,11 @@
 # Keystroke savings using a byte LLM to predict next words
 
-# Result: m1 macbook, 4/22/25
+# RESULTS: m1 macbook, 4/22/25
+# % ks_byte_cpu.sh
+#  TRUNCATED: 0
+#  TIME: 9452.81
+#  SECS/PRED: 4.4775
+#  FINAL KS: 50.1299
 
 MODEL_NAME=nllg/bygpt5-small-en
 PHRASES=comm_case_dev.txt
