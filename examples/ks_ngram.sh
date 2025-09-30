49958d6f1e39b003faf82048535ec4ccfc51b641
Keith Vertanen <kdv123@gmail.com>
2025-09-30 12:02:38 -0400

    fix to added files

--- examples/ks_ngram.sh
+++ examples/ks_ngram.sh
@@ -8,7 +8,7 @@
 
 LM=models/lm_dec19_char_tiny_12gram.arpa
 PHRASES=comm_case_dev.txt
-echo PHRASES ${PHRASES}
+echo PHRASES:
 wc -l -w ${PHRASES}
 
-../keystroke_savings.py --phrases ${PHRASES} --ngram --ngram-lm ${LM} --case-simple --nbest 5 --beam 2.0 --lower --strip-symbols
+../keystroke_savings.py --phrases ${PHRASES} --ngram --ngram-lm ${LM} --case-simple --nbest 5 --beam 2.0 --strip-symbols
