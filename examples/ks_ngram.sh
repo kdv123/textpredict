43299b17a6a4f1aefe59e9692b249daa87bfecaa
Keith Vertanen <kdv123@gmail.com>
2025-09-30 12:04:20 -0400

    fix to added files

--- examples/ks_ngram.sh
+++ examples/ks_ngram.sh
@@ -1,11 +1,5 @@
 # Keystroke savings using a n-gram to predict next words
 
-# Result: m1 macbook, 4/22/25
-# TRUNCATED: 0
-# TIME: 13.89
-# SECS/PRED: 0.0066
-# FINAL KS: 54.8311
-
 LM=models/lm_dec19_char_tiny_12gram.arpa
 PHRASES=comm_case_dev.txt
 echo PHRASES:
